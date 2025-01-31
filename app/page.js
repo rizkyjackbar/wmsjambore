4c1163253224b90b68abd269a46b6d5e378e4ee1
rizkyjackbar <muhamadrizky62020@students.amikom.ac.id>
2025-01-31 13:34:50 +0700

    Update title and content for WMS Jambore 2025

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
@@ -7,9 +5,14 @@ export default function Home() {
       <h1 className="text-4xl font-bold text-center mb-4">WMS Jambore 2025</h1>
 
       {/* Deskripsi Singkat */}
-      <p className="text-lg text-center max-w-2xl">
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      <p className="text-lg text-center max-w-2xl mb-8">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
+
+      {/* Footer */}
+      <footer className="text-sm text-center text-gray-500">
+        Made by rizkyjackbar
+      </footer>
     </div>
   );
 }
